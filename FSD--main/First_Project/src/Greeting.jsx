f0eb096a9382b0ce1ccc37950fe0bd707b8ef9ad
manish <niranjanmanish788@gmail.com>
2025-11-04 10:17:41 +0530

    greeting

--- FSD--main/First_Project/src/Greeting.jsx
+++ FSD--main/First_Project/src/Greeting.jsx
@@ -1,7 +1,7 @@
-import { usestate } from "react";
+import { Usestate } from "react";
 
 function Greeting(){
-    const [message, setMessage] = useState("Hello Class");
+    const [message, setMessage] = Usestate("Hello Class");
 
     function ChangeMessage(){
         setMessage("You  clicked the button!");
@@ -9,6 +9,7 @@ function Greeting(){
     return(
         <div>
             <h2>{message}</h2>
+            <h1> Learning React</h1>
             <button onclick={ChangeMessage}>Click Me</button>
         </div>
     );
